f0624b4f5d06b4b9d171cb428431b03b5f073331
jungsikjeong <wndtlr1024@gmail.com>
2022-08-14 15:06:02 +0900

    메인컨텐츠css작업

--- client/src/components/MainContents/ContentsItem.js
+++ client/src/components/MainContents/ContentsItem.js
@@ -1,11 +1,98 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { FaCommentAlt } from 'react-icons/fa';
+import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
 
-const ContentsItem = () => {
-  return <div>ContentsItem</div>;
+const Item = styled.li`
+  position: relative;
+  margin-bottom: 10px;
+  padding: 10px 30px;
+  background-color: #fff;
+`;
+
+const UpAndDownWrap = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-size: 16px;
+`;
+
+const CommunityName = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const PostTitle = styled.div`
+  margin-bottom: 10px;
+  font-size: 20px;
+`;
+
+const PostContents = styled.div`
+  margin-bottom: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22px;
+`;
+
+const ContentsItem = ({ item }) => {
+  const {
+    like,
+    communityName,
+    userName,
+    postTitle,
+    postContents,
+    date,
+    comments,
+  } = item;
+  return (
+    <Item>
+      <UpAndDownWrap>
+        <AiOutlineArrowUp />
+        {/* 추천횟수 */}
+        <span>{like}</span>
+        <AiOutlineArrowDown />
+      </UpAndDownWrap>
+      <CommunityName>
+        {/* 임시로 유저 아이콘 */}
+        <div
+          style={{
+            borderRadius: '50%',
+            background: 'black',
+            width: '15px',
+            height: '15px',
+            marginRight: '5px',
+          }}
+        />
+
+        <p>
+          /r/{communityName} •
+          <span style={{ opacity: '0.5' }}>
+            Posted by /u/{userName} &nbsp; {date}
+          </span>
+        </p>
+      </CommunityName>
+
+      <PostTitle>{postTitle}</PostTitle>
+
+      <PostContents>{postContents}</PostContents>
+
+      <div style={{ fontSize: '16px' }}>
+        <FaCommentAlt style={{ color: 'black', marginRight: '3px' }} />
+        <span style={{ fontSize: '17px', fontWeight: '400' }}>
+          {comments.length}
+        </span>
+      </div>
+    </Item>
+  );
 };
 
-ContentsItem.propTypes = {};
+ContentsItem.propTypes = {
+  item: PropTypes.object,
+};
 
 export default ContentsItem;
--- client/src/components/MainContents/ContentsList.js
+++ client/src/components/MainContents/ContentsList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { FaCommentAlt } from 'react-icons/fa';
-import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import dbArray from '../../data/db';
+
+import ContentsItem from './ContentsItem';
 
 const Container = styled.div`
   width: 100%;
@@ -10,82 +11,18 @@ const Container = styled.div`
 `;
 
 const List = styled.ul`
-  position: relative;
-  background-color: #fff;
   color: black;
   font-weight: bold;
   font-size: 13px;
-  padding: 10px 30px;
-`;
-
-const Item = styled.li``;
-
-const UpAndDownWrap = styled.div`
-  position: absolute;
-  top: 10px;
-  left: 10px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const CommunityName = styled.div`
-  width: 100%;
-  display: flex;
-  margin-bottom: 10px;
-`;
-
-const PostTitle = styled.div`
-  margin-bottom: 10px;
-  font-size: 20px;
-`;
-
-const PostContents = styled.div`
-  margin-bottom: 10px;
-  font-size: 14px;
-  font-weight: 600;
 `;
 
 const ContentsList = () => {
   return (
     <Container>
       <List>
-        <Item>
-          <UpAndDownWrap>
-            <AiOutlineArrowUp />
-            {/* 추천횟수 */}
-            <span>1</span>
-            <AiOutlineArrowDown />
-          </UpAndDownWrap>
-          <CommunityName>
-            {/* 임시로 유저 아이콘 */}
-            <div
-              style={{
-                borderRadius: '50%',
-                background: 'black',
-                width: '15px',
-                height: '15px',
-                marginRight: '5px',
-              }}
-            />
-
-            <p>
-              /r/커뮤니티 이름 •{' '}
-              <span style={{ opacity: '0.5' }}>
-                Posted by /u/유저이름 &nbsp; 2022-08-03 18:07
-              </span>
-            </p>
-          </CommunityName>
-
-          <PostTitle>포스트를 생성</PostTitle>
-
-          <PostContents>포스트 생성 설명</PostContents>
-
-          <div style={{ fontSize: '16px' }}>
-            <FaCommentAlt style={{ color: 'black', marginRight: '3px' }} />
-            <span style={{ fontSize: '20px', fontWeight: '400' }}>1</span>
-          </div>
-        </Item>
+        {dbArray.map((item) => (
+          <ContentsItem item={item} key={item.id} />
+        ))}
       </List>
     </Container>
   );
